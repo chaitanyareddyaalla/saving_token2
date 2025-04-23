23632e405463bf4ec64239551500001045a49946
Aalla Chaitanya Reddy <daxingamerz358@gmail.com>
2025-04-23 14:05:39 +0530

    done

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 const encrypt = (payload, secret) => {
   // your code here and return token
+  try{
+    const token = jwt.sign(payload,secret,{
+      expiresIn: '2hr'
+    })
+    console.log('sucess ')
+    return token
+  }catch(err){
+    throw new Error(`Error encrypting payload: ${err.message}`);
+  }
 };
 
 module.exports = encrypt;
